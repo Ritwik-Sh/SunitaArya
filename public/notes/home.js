052e56eca6e995fdb71c55c863659951fd9ec041
Ritwik Sharma <ritwiksharma2010@outlook.com>
2025-09-25 00:26:28 +0530

    1.5.1

--- public/notes/home.js
+++ public/notes/home.js
@@ -1,7 +1,9 @@
 notesList = [
     { title: "Skeletal System", path: "skeletalSystem.html", icon: "fa-solid fa-bone" },
     { title: "Muscles and Muscle Tissue", path: "load.html?doc=Muscles and Muscle Tissue.pdf", icon: "fa-solid fa-dumbbell" },,
-    { title: "Transcription, Translation and Termination", path: "transcription-translation-termination.html", icon: "fa-solid fa-dna"}
+    { title: "Transcription, Translation and Termination", path: "transcription-translation-termination.html", icon: "fa-solid fa-dna"},
+    { title: "Role of Government in Sustainable Development", path: "govInSusDev.html", icon: "fa-solid fa-city"},
+    { title: "Biosafety", path: "Biosafety.html", icon: "fa-solid fa-tree"}
 ]
 const noteContainer = document.getElementById('noteContainer');
 document.addEventListener('DOMContentLoaded', () => {
